933e343038ee3979cf9dd6bb99edb4f7baf58248
MD. Shohag Mia <mdshohagmia14@outlook.com>
2018-05-05 00:17:57 +0600

    Initial

--- StringPattern/Program.cs
+++ StringPattern/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringPattern
 {
@@ -10,6 +6,43 @@ namespace StringPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter The Three Character:");
+            string originalString = Console.ReadLine();
+            char[] arrayString = originalString.ToCharArray();
+            for (int i = 0; i < originalString.Length; i++)
+            {
+                try
+                {
+                    string dh = arrayString[i].ToString();
+                    Console.Write(dh);
+                    Console.Write(",");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            for (int i = 0; i < originalString.Length; i++)
+            {
+                for (int j = 0; j < arrayString.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        try
+                        {
+                            Console.Write(arrayString[i]);
+                            Console.Write(arrayString[j]);
+                            Console.Write(",");
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            Console.Write(originalString);
+            Console.ReadKey();
         }
     }
 }
